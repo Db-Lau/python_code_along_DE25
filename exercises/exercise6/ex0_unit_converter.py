d832bdbdff49b5df79054c27e6cd7c05f5e502fc
kokchun <kokchungiang@gmail.com>
2025-10-23 10:06:19 +0200

    ex0 init

--- exercises/exercise6/ex0_unit_converter.py
+++ exercises/exercise6/ex0_unit_converter.py
@@ -3,6 +3,15 @@ class UnitConverter:
     def __init__(self, value: int | float):
         self.value = value
 
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, new_value):
+        # validation code
+
+        self._value = new_value
 
 unit_converter = UnitConverter(5)
 
