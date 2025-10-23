6aafc8edb0a9ade7775bcc400aa6f423596746e5
kokchun <kokchungiang@gmail.com>
2025-10-23 10:10:23 +0200

    ex0 init

--- exercises/exercise6/ex0_unit_converter.py
+++ exercises/exercise6/ex0_unit_converter.py
@@ -1,3 +1,6 @@
+from numbers import Number
+
+
 class UnitConverter:
     # | means or
     def __init__(self, value: int | float):
@@ -6,15 +9,18 @@ class UnitConverter:
     @property
     def value(self):
         return self._value
-    
+
     @value.setter
     def value(self, new_value):
         # validation code
+        if not isinstance(new_value, Number):
+            raise TypeError(f"the value must be a Number not a {type(new_value)}")
 
         self._value = new_value
 
+
 unit_converter = UnitConverter(5)
 
-# shortcut to get this output: 
-# unit_converter.value = 5 
+# shortcut to get this output:
+# unit_converter.value = 5
 print(f"{unit_converter.value = }")
