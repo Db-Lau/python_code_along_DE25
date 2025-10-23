59cfef61e0aef275e1d01200f9487a3b996c048e
kokchun <kokchungiang@gmail.com>
2025-10-23 10:29:31 +0200

    refactor validation code

--- exercises/exercise6/ex0_unit_converter.py
+++ exercises/exercise6/ex0_unit_converter.py
@@ -1,11 +1,5 @@
 from numbers import Number
-
-def validate_positive_number(value) -> None:
-    if not isinstance(value, Number):
-        raise TypeError(f"the value must be a Number not a {type(value)}")
-
-    if value < 0:
-        raise ValueError(f"value can't be negative, not {value}")
+from utils import validate_positive_number
 
 
 class UnitConverter:
