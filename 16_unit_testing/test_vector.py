9dc89ad8444ad6583de0525115577d05435c109f
kokchun <kokchungiang@gmail.com>
2025-10-29 11:45:01 +0100

    test errors

--- 16_unit_testing/test_vector.py
+++ 16_unit_testing/test_vector.py
@@ -1,20 +1,29 @@
 from pytest import raises
 from vector import Vector
 
+
 def test_valid_init():
-    v = Vector(1,2)
-    assert v.numbers == (1,2)
+    v = Vector(1, 2)
+    assert v.numbers == (1, 2)
+
 
 # TODO: you guys implement these until 11:30 and take some break also
 
+
 # test negative values in init
+def test_negative_valid_init():
+    v = Vector(-1, -5, 3)
+    assert v.numbers == (-1, -5, 3)
 
 # test string in the init
+def test_string_in_init():
+    with raises(TypeError):
+        Vector("1")
+
 
 # test len() function
 
 # test abs() function
-
 
 
 def test_empty_vector_fail():
