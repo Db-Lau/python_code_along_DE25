794296b015e6802579bed3936290b245a07ab837
kokchun <kokchungiang@gmail.com>
2025-10-29 11:12:27 +0100

    unit testing init

---
+++ 16_unit_testing/test_vector.py
@@ -0,0 +1,21 @@
+from pytest import raises
+from vector import Vector
+
+def test_valid_init():
+    v = Vector(1,2)
+    assert v.numbers == (1,2)
+
+# test negative values in init
+
+
+# test string in the init
+
+
+# test len() function
+
+
+# test abs() function
+
+def test_empty_vector_fail():
+    with raises(ValueError):
+        Vector()
