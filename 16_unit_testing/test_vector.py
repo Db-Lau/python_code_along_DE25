126169ed6cfb98997177d10c46dc4618308afa0b
kokchun <kokchungiang@gmail.com>
2025-10-29 11:50:53 +0100

    test vectors different lengths

--- 16_unit_testing/test_vector.py
+++ 16_unit_testing/test_vector.py
@@ -15,13 +15,21 @@ def test_negative_valid_init():
     v = Vector(-1, -5, 3)
     assert v.numbers == (-1, -5, 3)
 
+
 # test string in the init
-def test_string_in_init():
+def test_string_in_init_fails():
     with raises(TypeError):
         Vector("1")
 
 
 # test len() function
+def test_different_lengths():
+    vectors = (Vector(1, 2), Vector(1), Vector(1, 2, 3), Vector(1, 2, 3, 4))
+    expected_lengths = (2, 1, 3, 4)
+
+    for vector, expected_length in zip(vectors, expected_lengths):
+        assert len(vector) == expected_length
+
 
 # test abs() function
 
