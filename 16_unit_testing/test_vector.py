9e48dee41a2ce0f38c5fca2f5e95026866d1971b
kokchun <kokchungiang@gmail.com>
2025-10-29 11:13:35 +0100

    unit testing init

--- 16_unit_testing/test_vector.py
+++ 16_unit_testing/test_vector.py
@@ -5,16 +5,17 @@ def test_valid_init():
     v = Vector(1,2)
     assert v.numbers == (1,2)
 
-# test negative values in init
+# TODO: you guys implement these until 11:30 and take some break also
 
+# test negative values in init
 
 # test string in the init
 
-
 # test len() function
 
-
 # test abs() function
+
+
 
 def test_empty_vector_fail():
     with raises(ValueError):
