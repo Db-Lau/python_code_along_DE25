b9610473533e18212eddfc6aeb97e3d5908264c9
kokchun <kokchungiang@gmail.com>
2025-10-30 10:35:31 +0100

    skeleeton

---
+++ exercises/tdd_exercise/triangle.py
@@ -0,0 +1,44 @@
+def validate_triangle(self):
+    pass
+
+
+class Triangle:
+    def __init__(self):
+        pass
+
+    @property
+    def base(self):
+        pass
+    
+    @base.setter
+    def base(self):
+        pass
+
+    @property
+    def height(self):
+        pass
+
+    @height.setter
+    def height(self):
+        pass
+
+    @property
+    def area(self):
+        pass  
+
+    @property
+    def perimeter(self):
+        pass  
+
+    def __eq__(self, other):
+        pass
+
+    def __lt__(self, other):
+        pass
+    
+    def __gt__(self, other):
+        pass
+
+    def __repr__(self):
+        pass
+
