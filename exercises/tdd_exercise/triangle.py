5fb3092fb9bdb30b9d4fc30c1f87a984c668a6fb
kokchun <kokchungiang@gmail.com>
2025-10-30 14:18:40 +0100

    refactor and fixed bool test

--- exercises/tdd_exercise/triangle.py
+++ exercises/tdd_exercise/triangle.py
@@ -1,20 +1,21 @@
 import numbers
+from utils import validate_number
 
 class Triangle:
     # x and y optional
     def __init__(self, base, height):
-        self.base = base 
+        self.base = base
         self.height = height
 
     @property
     def base(self):
         return self._base
-    
+
     @base.setter
     def base(self, value):
-        if not isinstance(value, numbers.Number):
-            raise TypeError("must be a number")
         
+        validate_number(value)
+
         if value <= 0:
             raise ValueError("base must be larger than zero")
         self._base = value
@@ -25,27 +26,26 @@ class Triangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, numbers.Number):
-            raise TypeError("must be a number")
+        validate_number(value)
+
         self._height = value
 
     @property
     def area(self):
-        pass  
+        pass
 
     # @property
     # def perimeter(self):
-    #     pass  
+    #     pass
 
     def __eq__(self, other):
         pass
 
     def __lt__(self, other):
         pass
-    
+
     def __gt__(self, other):
         pass
 
     def __repr__(self):
         pass
-
